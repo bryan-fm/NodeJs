c5c3a0eb14638d2e9ed3285e66913fb9caac7cc8
Bryan França <43351790+bryan-fm@users.noreply.github.com>
2023-01-11 18:42:52 -0300

    Update portal.js

--- Portal - exerc 1/portal.js
+++ Portal - exerc 1/portal.js
@@ -2,7 +2,25 @@ var http = require('http');
 
 var server = http.createServer( function(req, resp) {
 
-    resp.end("<html><body><h1> Portal de Noticias </h1></body></html>");
+    var categoria = req.url.replace("/","");
+
+    console.log(categoria);
+    var type = "<html><body><h1> Página Inicial </h1></body></html>";
+
+    if(categoria == 'tecnologia') {
+        type = "<html><body><h1> Portal de Noticias de Tec </h1></body></html>";
+    }
+
+    if(categoria == 'moda') {
+        type = "<html><body><h1> Portal de Noticias de Moda </h1></body></html>";
+    }
+
+    if(categoria == 'jogos') {
+        type = "<html><body><h1> Portal de Noticias de Jogos </h1></body></html>";
+    }
+
+    resp.end(type);
+    
 });
 
 server.listen(3000);
