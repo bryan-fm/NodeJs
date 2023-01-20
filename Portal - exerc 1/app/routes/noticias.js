0fa94a7c1a3914dfd363ea5ec28ff9d2cc0ffbc3
Bryan França <43351790+bryan-fm@users.noreply.github.com>
2023-01-20 00:02:42 -0300

    consultando banco de dados mysql

--- Portal - exerc 1/app/routes/noticias.js
+++ Portal - exerc 1/app/routes/noticias.js
@@ -1,3 +1,16 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app) {
-    app.get('/noticias', (req, resp) =>  resp.render("noticias/noticias"));
+
+    var connection = dbConnection();
+
+    app.get('/noticias', (req, resp) => {
+    
+        connection.query('select * from noticias',
+            function(error, result) {
+                //console.log(result);
+                resp.render("noticias/noticias", {noticias : result});
+            });
+
+    });
 };
